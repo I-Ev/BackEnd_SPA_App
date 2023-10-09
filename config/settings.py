a3875cb0ffc2961c9834a0062ae1e04c7c08341c
Ilya Evlanov <ilya.evlanov@gmail.com>
2023-10-09 14:50:48 +0300

    fix redis url

--- config/settings.py
+++ config/settings.py
@@ -162,10 +162,10 @@ CSRF_TRUSTED_ORIGINS = [
 CORS_ALLOW_ALL_ORIGINS = True
 
 # URL-адрес брокера сообщений
-CELERY_BROKER_URL = 'redis://localhost:6379'  # Например, Redis, который по умолчанию работает на порту 6379
+CELERY_BROKER_URL = 'redis://redis:6379/0'  # Например, Redis, который по умолчанию работает на порту 6379
 
 # URL-адрес брокера результатов, также Redis
-CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_RESULT_BACKEND = 'redis://redis:6379/0'
 
 # Флаг отслеживания выполнения задач
 CELERY_TASK_TRACK_STARTED = True
